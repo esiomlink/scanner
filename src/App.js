c774d23fd174c4cb728c34a5db573296bf238e6f
esiomlink <simon.ecoffet@gmail.com>
2021-06-05 16:48:59 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, useCallback, useEffect } from 'react';
+import BarcodeScannerComponent from 'react-qr-barcode-scanner';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState('Not Found');
+  const [devices, setDevices] = useState([]);
+  const [deviceId, setDeviceId] = useState({});
+  console.log(data)
+  console.log('tout les devices :',devices)
+  console.log('choix du device :',deviceId);
+
+
+
+// filtre seulement les cameras
+  const handleDevices = useCallback(
+    (mediaDevices) =>
+      setDevices(mediaDevices.filter(({ kind }) => kind === 'videoinput')),
+    [setDevices]
+  );
+
+  //sors toutes les devices
+  useEffect(() => {
+    navigator.mediaDevices.enumerateDevices().then(handleDevices);
+  }, [handleDevices]);
+
+  // mets la derniere camera en principal
+  useEffect(() => {
+      setDeviceId(devices[devices.length - 1]);
+  }, [devices]);
+
+  const videoConstraints = {
+    facingMode: 'environment',
+    aspectRatio: 1,
+    frameRate: 60,
+    focusDistance: 0,
+    resizeMode: 'crop-and-scale',
+    zoom: 0.1,
+    focusMode: 'manual',
+    deviceId: deviceId&&deviceId.deviceId,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='cam'>
+      {deviceId && (
+        <BarcodeScannerComponent
+          width={500}
+          height={500}
+          videoConstraints={videoConstraints}
+          onUpdate={(err, result) => {
+            if (result) setData(result.text);
+            else setData('Not Found');
+          }}
+        />
+      )}
+      <p>{data}</p>
+      <div className='selector-device'>
+        {devices &&
+          devices.map((device, key) => (
+            <div>
+              <input
+                id={device.deviceId}
+                value={device.deviceId}
+                name={device.deviceId}
+                type='checkbox'
+                onChange={(e) => setDeviceId(e.target.value)}
+              />
+              {device.label || `Device ${key + 1}`}
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
